6989075ceeaa019d23ee8fb8478ce6d9b1c63c8c
rick-zenith h <rickgrimes68p@gmail.com>
2024-08-22 17:38:02 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xd222A9Fda8915AdD941c4a2ee2E0A68102AE6d15'
+export const ProxyAdmin = '0xe24A6822E1AdCE1189A888dDFeb9543BeFE7f3cc'
+export const SWTRImplementation = '0x6bA8538502c3251Ee7efE97818e96e680fF24418'
     
